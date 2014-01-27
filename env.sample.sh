11425dd8433f115786f614e3cfae79c7f9336b78
Lucas Neves Martins <lucasmartins@railsnapraia.com>
2014-01-27 17:11:31 -0200

    Fixing lost :

--- env.sample.sh
+++ env.sample.sh
@@ -4,7 +4,7 @@
 export REDIS_URL='redis://localhost:6379'
 export SENDGRID_PASSWORD=''
 export SENDGRID_USERNAME=''
-export MONGODB_URL=mongodb://localhost:27017:mailcannon_development
+export MONGODB_URL=mongodb://localhost:27017/mailcannon_development
 export MAILCANNON_ENV=development
 
 cp config/mongoid.sample.yml config/mongoid.yml
