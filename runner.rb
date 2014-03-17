0e002a2a2658ed2115aeb0804da64a6714ac669d
João Hornburg <joao.hornburg@gmail.com>
2014-03-17 11:18:14 -0300

    fixing stuff

--- runner.rb
+++ runner.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.setup(:default)
 require 'mailcannon'
 require "kiqstand"
+require 'hirefire-resource'
 
 redis_url = ENV['REDIS_URL'] || 'redis://localhost:6379'
 MAILCANNON_ENV = ENV['MAILCANNON_ENV'] || 'development'
